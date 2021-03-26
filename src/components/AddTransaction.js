88edaf8376e99ae729ec864c02d7ab6fab30eba1
Nand Kishor <nandkk07@gmail.com>
2021-03-26 12:54:10 +0530

    init :fire:

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const AddTransaction = () => {
+    return (
+        <div>
+            <h3>Add new Transaction</h3>
+            <form>
+                <div className="form-control">
+                    <label>Text</label>
+                </div>
+            </form>
+        </div>
+    )
+}
