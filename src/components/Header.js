2308aee45b505034bce5b20de4291dc8a08a942f
Nand Kishor <nandkk07@gmail.com>
2021-03-27 13:57:56 +0530

    logic working :rocket:

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Header = () => {
     return (
         <div>
-            Expense Tracker
+            <h3>Expense Tracker</h3>
         </div>
     )
 }
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,20 +3,33 @@ import AppReducer from './AppReducer';
 
 const initialState ={
     transactions: [
-        {id:1, text:'Flower', amount:20},
-        {id:2, text:'Salary', amount:120},
-        {id:3, text:'Shopping', amount:-40},
-        {id:4, text:'Book', amount:-60}
+        {id:1, text:'Salary', amount:200}
     ]
 }
 
 export const GlobalContext = createContext(initialState);
 
 export const GlobalProvider = ({children}) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState)
+    const [state, dispatch] = useReducer(AppReducer, initialState);
+
+    function deleteTransaction(id) {
+        dispatch({
+            type: 'DELETE_TRANSACTION',
+            payload: id
+        });
+    }
+
+    function addTransaction(transaction) {
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
+        });
+    }
 
     return (<GlobalContext.Provider value={{
-        transactions: state.transactions
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction
     }}>
         {children}
     </GlobalContext.Provider>)
